2eab3ade0e45eb692b98e736c3085274afb1959b
Anna Soboleva <soboleva_a7@mail.ru>
2024-01-17 22:27:29 +0300

    Homework4 - Class

---
+++ Class/Creditcard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class;
+
+public class Creditcard
+{
+    public string AccountNumber;
+    public int SumNow;
+
+    public Creditcard(string number, int sum)
+    {
+        AccountNumber = number;
+        SumNow = sum;
+    }
+
+    public void CashNowDo()
+    {
+        Console.WriteLine($"На карте {AccountNumber} в настоящий момент {SumNow}");
+    }
+
+    public void PutCash(int cash)
+    {
+        SumNow += cash;
+        Console.WriteLine($"Вы внесли на счет {cash}");
+    }
+    public void WithdrawCash(int cash)
+    {
+        if (SumNow >= cash)
+        {
+            SumNow -= cash;
+            Console.WriteLine($"Вы сняли со счета {cash}");
+        }
+        else
+        {
+            Console.WriteLine("На счете недостаточно средств");
+        }
+    }
+    public void CashNow()
+    {
+        Console.WriteLine($"На карте {AccountNumber} в настоящий момент {SumNow}");
+    }
+}
---
+++ Class/Phone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class;
+
+public class Phone
+{
+    public string Number;
+    public string Model;
+    public double Weight;
+
+    public Phone() { }
+
+    public Phone(string number, string model)
+    {
+        Number = number;
+        Model = model;
+    }
+
+    public Phone(string number, string model, double weight) : this(number, model)
+    { Weight = weight; }
+
+    public void ReceiveCall(string name)
+    {
+        Console.WriteLine($"Вам звонит {name}");
+    }
+
+    public string GetNumber() => Number;
+
+    public void SendMessage(params string[] numbers)
+    {
+        foreach (string n in numbers)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
